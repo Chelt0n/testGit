b330b1baf647b302c9ba3b0a1dd9630bc07ea8be
Chelt0n <qys1@yandex.ru>
2021-12-26 11:23:10 +0300

    исправил 4 ветку

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            //4 ветка
+            //4  ща будет 5ветка
     }
 }
